2c75bfa2cab70f21bf46456a11c9426040aad813
tom wang <tong.wang@liferay.com>
2015-05-07 16:10:10 -0700

    simplified

--- testscript.js
+++ testscript.js
@@ -1,22 +1,24 @@
 function UserIn(email,firstName,lastName,userType,password) {
   popup();
   Parse.initialize("ej29LXB9zHARKwcF5gHhkQ4SnJS7mGwWZ01qrZAa", "jTpvM9KVA9G9XteMyDD4nDcL6xNPVhg44zliTSrw");
-    
-   var TestObject = Parse.Object.extend("WorkflowUser");
-    var testObject = new TestObject();
-    testObject.save({email: email}),
-      testObject.save({name: firstName},
-      testObject.save({LastName: lastName}),
-      testObject.save({UserType: userType}),
-      testObject.save({Password: password}), {
-      success: function(object) {
-        $(".success").show();
-      },
-      error: function(model, error) {
-        $(".error").show();
-      }
-    });
- }
+
+  var TestObject = Parse.Object.extend("WorkflowUser");
+  var testObject = new TestObject();
+  testObject.save({
+    email: email,
+    name: firstName,
+    LastName: lastName,
+    UserType: userType,
+    Password: password
+  }, {
+    success: function(object) {
+      $(".success").show();
+    },
+    error: function(model, error) {
+      $(".error").show();
+    }
+  });
+}
 function popup() {
   alert("don't push me")
 }
