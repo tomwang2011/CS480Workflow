7da1d968fa110b0354c1a08b139c27a82b9c81cd
tom <tomwang2011@gmail.com>
2015-06-06 09:00:43 -0700

    demo

--- ParseQuery.js
+++ ParseQuery.js
@@ -1,3 +1,13 @@
+ var callfunction = function(tableName,fieldA,valueA,fieldB,valueB,returnItemField,itemid) {
+    //call the Promise function (all Parse queries are promise functions)
+    getObjectUtil(tableName,fieldA,valueA,fieldB,valueB,function(result) {
+        //below is an example of setting the variable itemid to the object.id of field-name of table name
+        console.log(result[0]);
+        console.log(result[0].id);
+        document.getElementById(itemid).innerHTML=result[0].get(returnItemField);
+    });
+  }
+
 var getObjectUtil = function(table,fieldA,valueA,fieldB,valueB,callback) {
     Parse.initialize("ej29LXB9zHARKwcF5gHhkQ4SnJS7mGwWZ01qrZAa", "jTpvM9KVA9G9XteMyDD4nDcL6xNPVhg44zliTSrw");
 
